255a0aee742c352e91e8fc7309d0640966d03e1d
TANISHA-KAUSHAL <144940071+TANISHA-KAUSHAL@users.noreply.github.com>
2023-09-14 16:58:56 +0530

    Create QuestionAnswer.java

---
+++ QuestionAnswer.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+public class QuestionAnswer {
+    public static String question[] ={
+            "121 Divided by 11 is ?",
+            "What is the Next Prime Number after 7 ?",
+            "Solve 23 + 3 ÷ 3?",
+            "What is 6% Equals to?",
+            "How many days are there in a week?",
+            "How many seconds make one hour?"
+    };
+
+    public static String Choices[][] ={
+            {"11","10","19","18"},
+            {"13","12","14","11"},
+            {"24","25","26","27"},
+            {"0.06","0.6","0.006","0.0006"},
+            {"8","9","7","5"},
+            {"3500","3600","6000","3200"}
+    };
+
+    public static String correctAnswers[] ={
+            "11",
+            "11",
+            "24",
+            "0.06",
+            "7",
+            "3600"
+    };
+}
